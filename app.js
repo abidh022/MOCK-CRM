2c3fff9fc43b5bb8d3645c61871cab4f5e2dc270
abidh022 <mohammedabidh690@gmail.com>
2024-11-20 15:41:44 +0530

    check4

--- app.js
+++ app.js
@@ -1,41 +1,21 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const { MongoClient } = require('mongodb');
-const leadRoutes = require('./routes/leadRoutes');
-const contactRoutes = require('./routes/contactRoutes')
 const cors = require('cors');
-require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config();
 
-// MongoDB connection setup
-// const uri = process.env.MONGODB_URI;
-// const client = new MongoClient(uri);
-// let leadsCollection;
-// let contactsCollection;
-
-// async function connectToDatabase() {
-//     try {
-//         await client.connect();
-//         const db = client.db("crm"); // database name
-//         leadsCollection = db.collection("leads");
-//         contactsCollection = db.collection("contacts");
-//         console.log("Connected to MongoDB!");
-//     } catch (error) {
-//         console.error("Error connecting to MongoDB:", error);
-//         process.exit(1); // Exit process if connection fails
-//     }
-// }
-
-// connectToDatabase();
+const leadRoutes = require('./routes/leadRoutes');
+const contactRoutes = require('./routes/contactRoutes')
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use lead routes
 app.use('/leads', leadRoutes);

ec0cadeeb0bba00d67d65e919f3d82119739fe2f
abidh022 <mohammedabidh690@gmail.com>
2024-11-20 11:17:36 +0530

    app.js modify

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { MongoClient } = require('mongodb');
-const leadRoutes = require('./routes/leadRoutes');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
 
 let leadsCollection;
 let contactsCollection;
@@ -36,39 +36,40 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-// // Endpoint to retrieve leads
-// app.post('/data/leads', async (req, res) => {
-//     try {
-//         // await client.connect();
-//         const db = client.db("crm"); //database name
-//         leadsCollection = db.collection("leads");
-//         const result = await leadsCollection.insertOne(req.body);
+// Endpoint to retrieve leads
+app.post('/data/leads', async (req, res) => {
+    try {
+        // await client.connect();
+        const db = client.db("crm"); //database name
+        leadsCollection = db.collection("leads");
+        const result = await leadsCollection.insertOne(req.body);
         
-//         // Send only the insertedId in the response
-//         res.status(200).json({
-//             id: result.insertedId.toString()
-//         });
-//     } catch (error) {
-//         console.error('Error retrieving leads:', error);
-//         res.status(500).send('Error retrieving leads');
-//     }
-// });
-// //GET
-// app.get('/data/leads', async (req, res) => {
-//     try {
-//         const leads = await leadsCollection.find().toArray(); // Fetch all leads
-//         res.status(200).json(leads);
-//     } catch (error) {
-//         console.error('Error fetching leads:', error);
-//         res.status(500).send('Error fetching leads');
-//     }
-// });
+        // Send only the insertedId in the response
+        res.status(200).json({
+            id: result.insertedId.toString()
+        });
+    } catch (error) {
+        console.error('Error retrieving leads:', error);
+        res.status(500).send('Error retrieving leads');
+    }
+});
+//GET
+app.get('/data/leads', async (req, res) => {
+    try {
+        const leads = await leadsCollection.find().toArray(); // Fetch all leads
+        res.status(200).json(leads);
+    } catch (error) {
+        console.error('Error fetching leads:', error);
+        res.status(500).send('Error fetching leads');
+    }
+});
 
 
 app.use((req, res, next) => {
     req.leadsCollection = leadsCollection; // Add the leadsCollection to the request object
     next();
 });
+const leadRoutes = require('./routes/leadRoutes');
 
 app.use('/data/leads', leadRoutes); // Use lead routes 
 
